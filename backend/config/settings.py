1d1ef6e5495194ae6ce3ddeded4ffcfccec3c748
Fermion <amgainsamie99@gmail.com>
2026-07-06 22:36:04 +0545

    deployment changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,9 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [  "api.samiramgain.com.np",
+ALLOWED_HOSTS = [
+    "localhost",
+    "api.samiramgain.com.np",
     "ioemock-backend.onrender.com",]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
